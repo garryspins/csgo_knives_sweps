019afaa5e400ea0fceeedc5a49b1b7e31be30e8d
Garry but Spinning <orionspersonal1954@gmail.com>
2021-03-23 09:18:21 -0700

    Better Effect

--- lua/effects/csgo_knifeimpact.lua
+++ lua/effects/csgo_knifeimpact.lua
@@ -1,30 +1,34 @@
-function EFFECT:Init( data )
-  local vOffset = data:GetOrigin() -- + Vector( 0, 0, 0.2 )
-  -- local vAngle = data:GetAngles()
-  local emitter = ParticleEmitter( vOffset ) -- there was false
-    for i=0, 5 do
-      local particle = emitter:Add( "particle/particle_smokegrenade", vOffset )
-      if particle then
-        -- particle:SetAngles( vAngle )
-        particle:SetVelocity( math.random(12,16) * math.sqrt(i) * data:GetNormal() * 3 + 2 * VectorRand() )
-        particle:SetColor( 135, 135, 135 )
-        particle:SetLifeTime( 0 )
-        particle:SetDieTime( math.Rand( 0.5, 1.5 ) )
-        particle:SetStartAlpha( 255 )
-        particle:SetEndAlpha( 0 )
-        particle:SetStartSize( math.Rand( 5, 8 ) *math.Clamp(i,1,4) * 0.166 )
-        --particle:SetStartLength( 1 )
-        particle:SetEndSize( math.Rand( 16, 24 ) * math.sqrt(math.Clamp(i,1,4)) * 0.166 )
-        --particle:SetEndLength( 4 )
-        particle:SetRoll( math.Rand( -25, 25 ) )
-        particle:SetRollDelta( math.Rand( -0.05, 0.05 ) )
-      end
+-- <3 tomdotbat
+
+local emitter
+
+function EFFECT:Init(data)
+    local vOffset = data:GetOrigin()
+
+    if emitter then
+        emitter:SetPos(vOffset)
+    else
+        emitter = ParticleEmitter(vOffset)
     end
-  emitter:Finish()
+
+    for i = 0, 3 do
+        local particle = emitter:Add("particle/particle_smokegrenade", vOffset)
+        if not particle then break end
+        particle:SetVelocity(math.random(2, 8) * data:GetNormal() * 3 + 2 * VectorRand())
+        particle:SetColor(135, 135, 135)
+        particle:SetLifeTime(0)
+        particle:SetDieTime(math.Rand(0.5, 1.5))
+        particle:SetStartAlpha(100)
+        particle:SetEndAlpha(0)
+        particle:SetStartSize(math.Rand(2, 5) * math.Clamp(i, 1, 4) * 0.166)
+        particle:SetEndSize(math.Rand(16, 24) * math.Clamp(i, 1, 4) * 0.166)
+        particle:SetRoll(math.Rand(-25, 25))
+        particle:SetRollDelta(math.Rand(-0.05, 0.05))
+    end
 end
 
 function EFFECT:Think()
-  return false
+    return false
 end
 
 function EFFECT:Render()
